331e57813a1cfd45109b22f5c36d46706c88aa83
saningo <nickmentor421@gmail.com>
2019-12-27 11:39:14 +0300

    School portal App

---
+++ app/src/main/java/com/example/portalapp/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.portalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+private WebView webView;
+
+
+   private MainActivity activity ;
+    private ProgressDialog progDailog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+
+        activity = this;
+
+        progDailog = ProgressDialog.show(activity, "Loading","Please wait...", true);
+        progDailog.setCancelable(false);
+
+
+
+        webView = (WebView) findViewById(R.id.webview);
+
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                progDailog.show();
+                view.loadUrl(url);
+
+                return true;
+            }
+            @Override
+            public void onPageFinished(WebView view, final String url) {
+                progDailog.dismiss();
+            }
+        });
+
+        webView.loadUrl("http://portal.kafuco.ac.ke/");
+
+    }
+}
+
